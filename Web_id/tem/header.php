b2e83dfeb000e2b62ad766d7a7d6b131ad5428f5
TuyenGiang <nguyentuyengiangbn@gmail.com>
2017-06-20 21:22:54 +0700

    Trang web php co ban

---
+++ Web_id/tem/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Trang chủ - Website công nghệ thông tin</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
---
+++ Web_id/tem/menu.php
@@ -0,0 +1,14 @@
+<div id="header">
+		<div id="logo-web">
+			<img src="img/logo.jpg">
+		</div><!--logo-web-->
+		<div id="menu">
+			<ul>
+				<li><a href="index.php">HOME</a></li>
+				<li><a href="#">HỌC HTML</a></li>
+				<li><a href="#">HỌC CSS</a></li>
+				<li><a href="#">HỌC JAVACRIP</a></li>
+				<li><a href="#">LIÊN HỆ</a></li>
+			</ul>
+		</div><!--menu-->
+	</div><!--header-->
